b297b21c38b06d4bc9cd1a248a2ee7626c317cee
Chi Nguyen <nguyen.chi.byu@gmail.com>
2019-01-04 10:48:23 -0800

    removed unwanted file

--- django_full_stack/The_Wall/apps/message/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-from django.contrib import messages
-from apps.message.models import * 
-from .models import Message
-
-def wall(request):
-    return render(request, "message/wall.html")
